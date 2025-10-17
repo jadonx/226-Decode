803e4ab7e2ee7c4ad5368c54a83c11cc432f7b36
Z-BUSTER <121357436+Z-BUSTER@users.noreply.github.com>
2025-10-17 16:04:39 -0400

    Encoder testing, plus encoder class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testStuff/encoder.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.testStuff;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+
+import org.firstinspires.ftc.teamcode.AS5600Encoder;
+
+@Config
+@TeleOp(name = "Encoder testing")
+public class encoder extends OpMode {
+
+    AS5600Encoder encoderStuff;
+    public void init(){
+        encoderStuff = hardwareMap.get(AS5600Encoder.class, "encoder");
+
+    }
+    public void loop(){
+        telemetry.addData("Angle", encoderStuff.getAngleDegrees());
+        telemetry.update();
+    }
+
+
+
+
+}
