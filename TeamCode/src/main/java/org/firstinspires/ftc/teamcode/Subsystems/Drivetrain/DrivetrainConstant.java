9a269fbf23f5147cf8070d97765d7dd804fdb7c5
Thai Duong Cai <172953571+thaiduongcai@users.noreply.github.com>
2025-10-28 17:17:18 -0400

    Duong - 10/28/25 | FieldCentric with NextFTC

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain/DrivetrainConstant.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
+public class DrivetrainConstant {
+    public static final String frontLeftName = "front_left";
+    public static final String frontRightName = "front_right";
+    public static final String backLeftName = "back_left";
+    public static final String backRightName = "back_right";
+
+    public static final String IMUName = "imu";
+}
