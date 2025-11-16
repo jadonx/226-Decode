eae5bcd17081c7cab2058827efde9fe0edf50618
Thai Duong Cai <172953571+thaiduongcai@users.noreply.github.com>
2025-11-16 16:49:31 -0500

    Meepmeep installed + autonomous pathing + encoder (not fixed)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TurretEncoderTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TurretEncoderTester.java
@@ -3,15 +3,12 @@ package org.firstinspires.ftc.teamcode.Testing;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.Subsystems.Encoder.AS5600Encoder;
 
 @TeleOp (name = "TurretEncoder Tester", group = "Testing")
 public class TurretEncoderTester extends OpMode {
     AS5600Encoder turretEncoder;
-    Telemetry telemetry;
-
     @Override
     public void init() {
         turretEncoder = hardwareMap.get(AS5600Encoder.class, Constants.HMTurretEncoder);
@@ -19,9 +16,8 @@ public class TurretEncoderTester extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addData("Turret Angle (Degrees): ", turretEncoder.getTurretAngleDegrees());
-        telemetry.addData("Relative Turret Angle (Degrees): ", turretEncoder.getRelativeTurretAngleDegrees());
-        telemetry.addData("Get Current Position: ", turretEncoder.getCurrentPosition());
+        telemetry.addData("Turret Angle (Degrees): ", turretEncoder.getTurretAngle());
+        telemetry.addData("Turret Angle (Degrees): ", turretEncoder.getAngleDegrees());
         telemetry.update();
     }
 }
