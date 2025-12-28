dd26ae1fffe259e7f8d933efa594b5346184bb53
Jadon Xu <jadonjxu@gmail.com>
2025-12-28 13:55:53 -0500

    added new pinpoint offsets

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/NewPinPoint.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.Constants;
+
+public class NewPinPoint {
+    private GoBildaPinpointDriver pinpoint;
+
+    public NewPinPoint(HardwareMap hardwareMap) {
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, Constants.HMPinPointer);
+        configurePinPoint();
+        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
+    }
+
+    public void configurePinPoint() {
+        pinpoint.setOffsets(-195.325, 2.671, DistanceUnit.MM);
+
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+        pinpoint.resetPosAndIMU();
+    }
+
+    public void update() {
+        pinpoint.update();
+    }
+
+    public Pose2D getPose() {
+        return pinpoint.getPosition();
+    }
+}
