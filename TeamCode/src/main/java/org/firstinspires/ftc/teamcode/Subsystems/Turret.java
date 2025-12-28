dcf2278882916571fd0bdfedeed536f6c35820ab
Thai Duong Cai <172953571+thaiduongcai@users.noreply.github.com>
2025-12-28 17:47:36 -0500

    Turret without limitation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Turret.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -22,6 +21,9 @@ public class Turret {
     public static double kI = 0.0; // Integral gain on position error
     public static double kD = 0.0005; // Derivative gain on position error
     public static double kF = 0.0015; // Feedforward to overcome static friction
+
+    public static double RIGHT_LIMIT = 140;
+    public static double LEFT_LIMIT  = -220;
 
     public Turret(HardwareMap hardwareMap) {
         turretRight = hardwareMap.get(CRServo.class, Constants.HMServoTurretRight);
@@ -64,6 +66,7 @@ public class Turret {
         while (angle > 180) angle -= 360;
         return angle;
     }
+
 
     public void setPower(double power) {
         turretLeft.setPower(-power);
