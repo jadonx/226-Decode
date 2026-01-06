cb839fbbc899d8890ba8c05598d4956160c808ef
Jadon Xu <jadonjxu@gmail.com>
2026-01-06 16:52:03 -0500

    added color sensor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ColorSensorsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ColorSensorsTest.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.Testing;
 
+import android.graphics.Color;
+
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Constants;
 
 @TeleOp(name="Color Sensors Test")
@@ -13,10 +16,27 @@ public class ColorSensorsTest extends OpMode {
     @Override
     public void init() {
         colorSensorFront = hardwareMap.get(RevColorSensorV3.class, Constants.HMFrontColorSensor);
+        colorSensorBack = hardwareMap.get(RevColorSensorV3.class, Constants.HMBackColorSensor);
     }
 
     @Override
     public void loop() {
+        telemetry.addData("Front HSV ", getHSVRev(colorSensorFront)[0]);
+        telemetry.addData("Front Distance ", colorSensorFront.getDistance(DistanceUnit.INCH));
+        telemetry.addData("Back HSV ", getHSVRev(colorSensorBack)[0]);
+        telemetry.addData("Back Distance ", colorSensorBack.getDistance(DistanceUnit.INCH));
 
+        telemetry.update();
+    }
+
+    public float[] getHSVRev(RevColorSensorV3 colorSensor) {
+        int r = colorSensor.red();
+        int g = colorSensor.green();
+        int b = colorSensor.blue();
+
+        float[] hsv = new float[3];
+        Color.RGBToHSV(r, g, b, hsv);
+
+        return hsv;
     }
 }
